4c5139622ef1aacf14cccabb1ce8934ca7296faf
ChandanSingla108 <chandansingla411@gmail.com>
2025-10-13 00:14:13 +0530

    Delete app directory

--- app/providers.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import type React from "react"
-
-import { AppStateProvider } from "@/context/app-state"
-
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <AppStateProvider>{children}</AppStateProvider>
-}
